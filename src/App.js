de024156e02648bcd8b99093e4a7e6d4e6902f8d
diegograssino <diegograssino@gmail.com>
2021-07-17 20:57:16 -0300

    Agregando Google Fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap CSS
 import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Button>Bootstrap</Button>
+      <h4 className="cursiveFont d1">Lima</h4>
+      <h4 className="titleFont h1">Titulo</h4>
+      <p>Parrafo 1234567890</p>
     </div>
   );
 }
