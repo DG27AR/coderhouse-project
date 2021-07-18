16e8aca275086f87ec18cdc476f07c4a558d9739
diegograssino <diegograssino@gmail.com>
2021-07-18 00:03:18 -0300

    Agregando paleta de colores a CSS y limpiando algunos archivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap CSS
 import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <h4 className="cursiveFont d1">Lima</h4>
-      <h4 className="titleFont h1">Titulo</h4>
-      <p>Parrafo 1234567890</p>
+    <div>
+      <h4 className="fontCursive d1">Lima</h4>
+      <h4 className="fontTitle h1">Titulo</h4>
+      <p className="colorGray">Parrafo 1234567890</p>
     </div>
   );
 }
