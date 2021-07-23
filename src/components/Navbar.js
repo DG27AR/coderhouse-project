1438ce0162fa857b3434fb0c3ff50d1a6daa51be
diegograssino <diegograssino@gmail.com>
2021-07-23 11:24:01 -0300

    Imagen del sitio modificada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,19 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaHome, FaSignInAlt } from 'react-icons/fa';
-import { SiCodechef } from 'react-icons/si';
+import { Navbar, Container } from 'react-bootstrap';
+import { BiCart } from 'react-icons/bi';
+import { default as myLogo } from '../assets/logo.svg';
 
 function MyNavbar() {
   return (
-    <Navbar bg="mediumblue" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <span className="fontCursive colorLightGrey h2">
-            <SiCodechef className="me-1" /> Lo de Rodo
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">
-              <FaSignInAlt className="me-1" />
-              Ingresar
-            </Nav.Link>
-            <hr class="dropdown-divider colorLightGrey"></hr>
-            <Nav.Link href="#home">
-              <FaHome className="me-1" />
-              Inicio
-            </Nav.Link>
-            <Nav.Link href="#features">Promociones</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand className="colorLightGrey fontCursive fs-3" href="#home">
+            <img alt="" src={myLogo} width="30" height="30" className="d-inline-block align-top me-1" /> LO DE RODO
+          </Navbar.Brand>
+          <BiCart className="colorLightGrey fs-3" />
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
